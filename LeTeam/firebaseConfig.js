a523c72b2d631f55ea1f80f800fb202fde6d9e07
Travis Ha <travisha33@gmail.com>
2024-11-26 02:08:17 -0800

    Added addevents tab, able to add event name and event info to Firebase realtime database

---
+++ LeTeam/firebaseConfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app';
+import { getDatabase } from 'firebase/database';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCTnCwSPs14P9s34rJXIrCrNA5-nW1RkV0",
+    authDomain: "leteam-a9091.firebaseapp.com",
+    databaseURL: "https://leteam-a9091-default-rtdb.firebaseio.com",
+    projectId: "leteam-a9091",
+    storageBucket: "leteam-a9091.firebasestorage.app",
+    messagingSenderId: "568744918599",
+    appId: "1:568744918599:web:f643d0fea43a60276dbb62",
+    measurementId: "G-LR2KSTW1SZ"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  
+  export { db };
